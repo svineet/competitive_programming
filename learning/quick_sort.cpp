1e4ed820af58f886eabf51e8e332dcdae316a4c0
svineet <saivineet89@gmail.com>
2020-02-24 05:03:37 +0530

    more updates

--- learning/quick_sort.cpp
+++ learning/quick_sort.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
 using namespace std;
 
-void partition(int A[], int left, int right) {
+int partition(int A[], int left, int right) {
     // Partition A into three parts
     // with A[left] as pivot
+    
+    int piv = A[left];
+    int i = left+1;
+    for (int j=left+1; j<=right; j++) {
+        if (A[j] < piv) {
+            swap(A[i], A[j]);
+            i++;
+        }
+    }
+
+    swap(A[left], A[i-1]);
+    return i-1;
 }
 
 void quick_sort(int A[], int left, int right) {
+    if (left >= right)
+        return;
+
+    int pivot_position = partition(A, left, right);
+    quick_sort(A, left, pivot_position);
+    quick_sort(A, pivot_position+1, right);
 }
 
 int main(int argc, char **argv) {
@@ -14,6 +32,12 @@ int main(int argc, char **argv) {
     int arr[n];
     for (int i=0; i<n; i++)
         cin >> arr[i];
+
+    quick_sort(arr, 0, n-1);
+
+    for (int i=0; i<n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
 
     return 0;
 }
